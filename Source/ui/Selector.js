fbc0d916339c95c5ae20b63000034d3cc95a81d7
Jason Young <suedama1756@hotmail.com>
2012-06-27 12:51:40 +0100

    Fixed issue with clearing nodes in selector

--- Source/ui/Selector.js
+++ Source/ui/Selector.js
@@ -84,6 +84,17 @@ photon.defineType(
             this.update();
         },
         update:function () {
+            var self = this;
+            if (!self.isUpdating_) {
+               self.isUpdating_ = true;
+               setTimeout(function() {
+                  self.updateCore_();
+               }, 0);
+           }
+        },
+        updateCore_ : function() {
+            this.isUpdating_ = false;
+
             var target = this.target_;
 
             // must store before clearing the dom
@@ -91,7 +102,7 @@ photon.defineType(
 
             // clear current items
             while (target.firstChild) {
-                target.remove(target.firstChild);
+                target.removeChild(target.firstChild);
             }
 
             if (this.items_) {
