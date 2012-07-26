64dd3bf6b92cc619d9864922930742b1f970575f
Jason Young <suedama1756@hotmail.com>
2012-07-26 19:02:37 +0100

    Added example for coercers

--- examples/Modelling.js
+++ examples/Modelling.js
@@ -28,19 +28,19 @@ require(["exampleWidget"], function (exampleWidget) {
         }
 
         example.Point = photon.observable.model.define({
-            x : {
-                initialValue: 0,
-                coerce: coerceNumber
+            x:{
+                initialValue:0,
+                coerce:coerceNumber
             },
-            y : {
-                initialValue: 0,
-                coerce: coerceNumber
+            y:{
+                initialValue:0,
+                coerce:coerceNumber
             }
         });
     }
 
     function coercedPointPageScript(photon, example) {
-        $(function() {
+        $(function () {
             photon.binding.applyBindings(new example.Point());
         });
     }
@@ -63,10 +63,56 @@ require(["exampleWidget"], function (exampleWidget) {
     function passingInitialValuesScript(photon, example) {
         // create a new Point initialing x to 3, and y to 4
         var point = new example.Point({
-                x:3,
-                y:4
-            });
+            x:3,
+            y:4
+        });
     }
+
+    function multipleCoercersScript(photon, example) {
+        function trim(value) {
+            return value ? photon.string.trim(value) : '';
+        }
+
+        function upper(value) {
+            var text = String(value);
+            return text ? text.toUpperCase() : '';
+        }
+
+        example.Person = photon.observable.model.define({
+            name:{
+                initialValue:'',
+                coerce:[trim, upper]
+            }
+        });
+    }
+
+    function sharedCoercersScript(photon, exanmple) {
+        photon.observable.model.coercers.upper = function (value) {
+            var text = String(value);
+            return text ? text.toUpperCase() : '';
+        }
+
+        photon.observable.model.coercers.trim = function (value) {
+            return value ? photon.string.trim(value) : '';
+        }
+    }
+
+    function sharedCoercersPersonScript(photon, example) {
+        example.Person = photon.observable.model.define({
+            name:{
+                initialValue:'',
+                coerce:['trim', 'upper']
+            }
+        });
+    }
+
+
+    function personPageScript(photon, example) {
+        $(function () {
+            photon.binding.applyBindings(new example.Person());
+        });
+    }
+
 
     exampleWidget.add(
         [
@@ -129,6 +175,43 @@ require(["exampleWidget"], function (exampleWidget) {
                     }
                 ],
                 html:'exampleCoercers1'
+            },
+            {
+                id:'exampleChainingCoercers',
+                javaScript:[
+                    {
+                        title:'Person.js',
+                        code:multipleCoercersScript,
+                        isRunnable:true
+                    },
+                    {
+                        title:'Page.js',
+                        code:personPageScript,
+                        isRunnable:true
+                    }
+                ],
+                html:'exampleChainingCoercers'
+            } ,
+            {
+                id:'exampleSharedCoercers',
+                javaScript:[
+                    {
+                        title:'Coercers.js',
+                        code:sharedCoercersScript,
+                        isRunnable:true
+                    },
+                    {
+                        title:'Person.js',
+                        code:sharedCoercersPersonScript,
+                        isRunnable:true
+                    },
+                    {
+                        title:'Page.js',
+                        code:personPageScript,
+                        isRunnable:true
+                    }
+                ],
+                html:'exampleSharedCoercers'
             }
         ]
     )
