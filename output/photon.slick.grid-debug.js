dd0c005680a4087fc9427750e9606570a872fab7
Jason Young <suedama1756@hotmail.com>
2012-09-13 22:33:08 +0100

    Updated build of slick plugin (untested)

--- output/photon.slick.grid-debug.js
+++ output/photon.slick.grid-debug.js
@@ -1,15 +1,15 @@
 (function(document){
     (function(factory) {
         if (typeof define === 'function' && define.amd) {
-            define(['exports', 'photon', 'jquery', 'slick.core', 'slick.grid'], factory);
+            define(['exports', 'photon', 'slick.core', 'jquery', 'slick.grid'], factory);
         } else if (window) {
             var nsi = 'photon.slickGrid'.split('.'), ns = window;
             for (var i= 0, n=nsi.length; i<n; i++) {
                 ns = ns[nsi[i]] = ns[nsi[i]] || {};
             }
-            factory(ns, window.photon, window.jQuery);
+            factory(ns, window.photon, window.Slick, window.jQuery);
         }
-    })(function(slickGrid, photon, $) {
+    })(function(slickGrid, photon, slick, $) {
         photon.slickGrid = photon.slickGrid || {};
         
         photon.slickGrid.create = function (container, data, columns, options) {
