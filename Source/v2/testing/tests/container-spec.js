d473a3bb1ec8e2c44cceabc7ff30c95c70df07ba
Jason Young <jyoung1756@gmail.com>
2012-12-21 13:09:12 +0000

    More container tests

--- Source/v2/testing/tests/container-spec.js
+++ Source/v2/testing/tests/container-spec.js
@@ -5,44 +5,82 @@ describe('container', function () {
         }
     }
 
+    function addDependencies(fn, dependencies) {
+        fn.$dependencies = dependencies;
+        return fn;
+    }
+
     function buildContainer(configure) {
         return photon.container([photon.module(configure)]);
     }
 
     describe('when resolving', function () {
         describe('from factory', function () {
+            function resolveExplicit(contract) {
+                var container = buildContainer(function (builder) {
+                    builder.factory('A', valueFactory('A'));
+                    builder.factory('B', ['A', function (a) {
+                        return a + 'B';
+                    }]);
+                    builder.factory('C', ['A', 'B', function (a, b) {
+                        return a + b + 'C';
+                    }]);
+                });
+                return container.resolve(contract);
+            }
+
+            function resolveAttributed(contract) {
+                var container = buildContainer(function (builder) {
+                    builder.factory('A', valueFactory('A'));
+                    builder.factory('B', addDependencies(function (a) {
+                        return a + 'B';
+                    }, ['A']));
+                    builder.factory('C', addDependencies(function (a, b) {
+                            return a + b + 'C';
+                        }, ['A', 'B']));
+                });
+                return container.resolve(contract);
+            }
+
             describe('with no dependencies', function () {
                 it('should resolve', function () {
-                    var container = buildContainer(function (builder) {
-                        builder.factory('A', valueFactory('X'));
-                    });
-                    expect(container.resolve('A')).toBe('X');
+                    expect(resolveExplicit('A')).toBe('A');
                 });
             });
 
-            describe('with resolvable explicit dependencies', function () {
+            describe('with one resolvable explicit dependency', function () {
                 it('should resolve', function () {
-                    var container = buildContainer(function (builder) {
-                        builder.factory('A', valueFactory('X'));
-                        builder.factory('B', ['A', function (a) {
-                            return a + a;
-                        }]);
-                    });
-                    expect(container.resolve('B')).toBe('XX');
+                    expect(resolveExplicit('B')).toBe('AB');
                 });
             });
 
-            describe('with resolvable attributed dependencies', function () {
+            describe('with one resolvable attributed dependency', function () {
                 it('should resolve', function () {
-                    var container = buildContainer(function (builder) {
-                        builder.factory('A', valueFactory('X'));
-                        builder.factory('B', photon.extend(function (a) {
-                            return a + a;
-                        }, {
-                            $dependencies: ['A']
-                        }));
-                    });
-                    expect(container.resolve('B')).toBe('XX');
+                    expect(resolveAttributed('B')).toBe('AB');
+                });
+            });
+
+            describe('with many resolvable explicit dependencies', function () {
+                it('should resolve', function () {
+                    expect(resolveExplicit('C')).toBe('AABC');
+                });
+            });
+
+            describe('with many resolvable attributed dependencies', function () {
+                it('should resolve', function () {
+                    expect(resolveAttributed('C')).toBe('AABC');
+                });
+            });
+
+            describe('with explicit and attributed dependencies', function() {
+                it ('should prioritize explicit dependencies', function() {
+                    var resolved = buildContainer(function(builder) {
+                        builder.factory('A', valueFactory('A'));
+                        builder.factory('B', ['A', addDependencies(function (a) {
+                            return a + 'B';
+                        }, ['C'])]);
+                    }).resolve('B');
+                    expect(resolved).toBe('AB');
                 });
             });
         });
@@ -61,18 +99,47 @@ describe('container', function () {
                 this.value = 'A';
             }
 
-            function TypeB(dependency) {
-                this.value = 'B' + (dependency && dependency.value);
+            function TypeB(a) {
+                this.value = (a && a.value) + 'B';
+            }
+
+            function TypeC(a, b) {
+                this.value = (a && a.value) + (b && b.value) + 'C';
+            }
+
+            function resolveExplicit(contract) {
+                // remove previously attributed dependencies
+                delete TypeA.$dependencies;
+                delete TypeB.$dependencies;
+                delete TypeC.$dependencies;
+
+                var container = buildContainer(function (builder) {
+                    builder.type('A', TypeA);
+                    builder.type('B', ['A', TypeB]);
+                    builder.type('C', ['A', 'B', TypeC]);
+                });
+
+                return container.resolve(contract);
+            }
+
+            function resolveAttributed(contract) {
+                var container = buildContainer(function (builder) {
+                    builder.type('A', TypeA);
+                    builder.type('B', photon.extend(TypeB, {
+                        $dependencies: ['A']
+                    }));
+                    builder.type('C', photon.extend(TypeC, {
+                        $dependencies: ['A', 'B']
+                    }));
+                });
+
+                return container.resolve(contract);
             }
 
             describe('with no dependencies', function () {
                 var resolved;
-                beforeEach(function() {
-                    var container = buildContainer(function (builder) {
-                        builder.type('A', TypeA);
-                    });
-
-                    resolved = container.resolve('A');
+                beforeEach(function () {
+                    resolved = resolveExplicit('A');
                 });
 
                 it('should resolve', function () {
@@ -84,19 +151,14 @@ describe('container', function () {
                 });
             });
 
-            describe('with resolvable explicit dependencies', function () {
+            describe('with one resolvable explicit dependency', function () {
                 var resolved;
-                beforeEach(function() {
-                    var container = buildContainer(function (builder) {
-                        builder.type('A', TypeA);
-                        builder.type('B', ['A', TypeB]);
-                    });
-
-                    resolved = container.resolve('B');
+                beforeEach(function () {
+                    resolved = resolveExplicit('B');
                 });
 
                 it('should resolve', function () {
-                    expect(resolved && resolved.value).toBe('BA');
+                    expect(resolved && resolved.value).toBe('AB');
                 });
 
                 it('should be of the correct type', function () {
@@ -104,25 +166,58 @@ describe('container', function () {
                 });
             });
 
-            describe('with resolvable attributed dependencies', function () {
+            describe('with one resolvable attributed dependency', function () {
                 var resolved;
-                beforeEach(function() {
-                    var container = buildContainer(function (builder) {
-                        builder.type('A', TypeA);
-                        builder.type('B', photon.extend(TypeB, {
-                            $dependencies: ['A']
-                        }));
-                    });
-
-                    resolved = container.resolve('B');
+                beforeEach(function () {
+                    resolved = resolveAttributed('B');
                 });
 
                 it('should resolve', function () {
-                    expect(resolved && resolved.value).toBe('BA');
+                    expect(resolved && resolved.value).toBe('AB');
                 });
 
                 it('should be of the correct type', function () {
                     expect(resolved instanceof  TypeB).toBe(true);
+                });
+            });
+
+            describe('with many resolvable explicit dependencies', function () {
+                var resolved;
+                beforeEach(function () {
+                    resolved = resolveExplicit('C');
+                });
+
+                it('should resolve', function () {
+                    expect(resolved && resolved.value).toBe('AABC');
+                });
+
+                it('should be of the correct type', function () {
+                    expect(resolved instanceof  TypeC).toBe(true);
+                });
+            });
+
+            describe('with many resolvable attributed dependencies', function () {
+                var resolved;
+                beforeEach(function () {
+                    resolved = resolveAttributed('C');
+                });
+
+                it('should resolve', function () {
+                    expect(resolved && resolved.value).toBe('AABC');
+                });
+
+                it('should be of the correct type', function () {
+                    expect(resolved instanceof  TypeC).toBe(true);
+                });
+            });
+
+            describe('with explicit and attributed dependencies', function() {
+                it ('should prioritize explicit dependencies', function() {
+                    var resolved = buildContainer(function(builder) {
+                        builder.type('A', TypeA);
+                        builder.type('B', ['A', addDependencies(TypeB, ['C'])]);
+                    }).resolve('B');
+                    expect(resolved && resolved.value).toBe('AB');
                 });
             });
         });
